48753f69d33b4343b8981cff9f3c51bfb7979cf7
icxz1 <alfianadityadwi@gmail.com>
2022-12-19 20:14:42 +0700

    menambahkan fitur menu login sebagai admin user dan menambah fitur exit

--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
+	// "log"
 )
 
 func main() {
 	// variable menu
 	var menuLogin int = 1
 
-
 	for menuLogin != 0 {
 		fmt.Println("1. Login Sebagai Admin")
 		fmt.Println("2. Login Sebagai Pegawai")
-		fmt.Println("9. Exit")
+		fmt.Println("0. Exit")
 		fmt.Scanln(&menuLogin)
+		if menuLogin == 1 {
+			fmt.Println("LOGIN SEBAGAI ADMIN")
+			fmt.Println("Masukkan Nama: ")
+			fmt.Scanln()
+			fmt.Println("Masukkan Password: ")
+			fmt.Scanln()
+		} else if menuLogin == 2 {
+			fmt.Println("LOGIN SEBAGAI PEGAWAI")
+			fmt.Println("Masukkan Nama: ")
+			fmt.Scanln()
+			fmt.Println("Masukkan Password: ")
+			fmt.Scanln()
+		} else if menuLogin == 0 {
+			break
+		}
 	}
 }
