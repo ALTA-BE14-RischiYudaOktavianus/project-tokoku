8b2c1ac96aebc3318d159b65737bd413f7499db7
icxz1 <alfianadityadwi@gmail.com>
2022-12-19 20:04:35 +0700

    menambahkan fitur menu login

--- main.go
+++ main.go
@@ -6,5 +6,14 @@ import (
 )
 
 func main() {
-	
+	// variable menu
+	var menuLogin int = 1
+
+
+	for menuLogin != 0 {
+		fmt.Println("1. Login Sebagai Admin")
+		fmt.Println("2. Login Sebagai Pegawai")
+		fmt.Println("9. Exit")
+		fmt.Scanln(&menuLogin)
+	}
 }
